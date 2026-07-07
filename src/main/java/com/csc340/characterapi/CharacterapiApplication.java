5e26ed1cd196af929a9c228d5a78904ed9cc1099
Shafath Moreano <shafathmoreano16@gmail.com>
2026-07-06 20:20:18 -0400

    Add Spring Boot set up

---
+++ src/main/java/com/csc340/characterapi/CharacterapiApplication.java
@@ -0,0 +1,13 @@
+package com.csc340.characterapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharacterapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CharacterapiApplication.class, args);
+	}
+
+}
